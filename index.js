c1ea8aca872be3cf5d68ae72d0458f9fb4359b79
masumbillahwebdeveloper <masumbillah.webdeveloper@gmail.com>
2023-05-20 23:35:39 +0600

    sorting done on mongo db

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app =express();
 const port = process.env.PORT || 5000;
@@ -56,6 +56,56 @@ async function run() {
         const result= await toyCollection.find().limit(20).toArray();
         res.send(result);
       });
+
+
+      app.get('/toy/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const options = {
+        projection: { toyName: 1, sellerName: 1, sellerEmail: 1, toyPicture: 1, price: 1,rating:1,quantity:1,detail:1 }
+      }
+      
+
+      const result = await toyCollection.findOne(query, options);
+      res.send(result);
+    })
+
+      app.get('/toy-edit/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+
+      const options = {
+        projection: { price: 1,quantity:1,detail:1 }
+      }
+
+      const result = await toyCollection.findOne(query, options);
+      res.send(result);
+    })
+    app.delete('/toy-delete/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) }
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    
+    app.patch('/toy-update/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updatedToy = req.body;
+      console.log(updatedToy);
+      const updateDoc = {
+        $set: {
+          price: updatedToy.price,
+          quantity: updatedToy.quantity,
+          detail: updatedToy.detail,
+        },
+      };
+      const result = await toyCollection.updateOne(filter, updateDoc)
+      res.send(result);
+
+    })
       
     app.get("/all-toy/:cat", async (req, res) => {
 
@@ -65,8 +115,12 @@ async function run() {
       });
 
 
-      app.get("/my-toy/:email", async(req,res)=>{
-        const result= await toyCollection.find({sellerEmail:req.params.email}).toArray();
+      app.get("/my-toy/descending/:email", async(req,res)=>{
+        const result= await toyCollection.find({sellerEmail:req.params.email}).sort({ price: -1 }).toArray();
+        res.send(result);
+      })
+      app.get("/my-toy/ascending/:email", async(req,res)=>{
+        const result= await toyCollection.find({sellerEmail:req.params.email}).sort({ price: 1 }).toArray();
         res.send(result);
       })
 
